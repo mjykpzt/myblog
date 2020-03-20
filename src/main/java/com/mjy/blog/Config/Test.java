6ca493034287269743b5bf63cd95a9f475cf9d24
mjykpzt <44693366+mjykpzt@users.noreply.github.com>
2020-03-20 17:22:56 +0800

    .

---
+++ src/main/java/com/mjy/blog/Config/Test.java
@@ -0,0 +1,27 @@
+package com.mjy.blog.Config;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * @author mjy
+ * @create 2020-03-20-16:53
+ */
+public class Test extends TextWebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        super.handleMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
