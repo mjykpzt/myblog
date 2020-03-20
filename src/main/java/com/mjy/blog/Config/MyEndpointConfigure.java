ec456380b90a5bdd640aa9ab914b1a891ee9620f
mjykpzt <44693366+mjykpzt@users.noreply.github.com>
2020-03-20 23:54:48 +0800

    .

--- src/main/java/com/mjy/blog/Config/MyEndpointConfigure.java
+++ src/main/java/com/mjy/blog/Config/MyEndpointConfigure.java
@@ -1,9 +1,5 @@
 package com.mjy.blog.Config;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
@@ -15,22 +11,7 @@ import javax.websocket.server.ServerEndpointConfig;
  * @create 2020-03-19-21:33
  */
 @Component
-public class MyEndpointConfigure extends ServerEndpointConfig.Configurator  {
-    private static volatile ApplicationContext context;
-
-//    @Override
-//    public <T> T getEndpointInstance(Class<T> clazz) throws InstantiationException {
-//        System.out.println(clazz);
-//        System.out.println(context.getBean(clazz));
-//        return context.getBean(clazz);
-//    }
-//1
-//    @Override
-//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-//        System.out.println("---------------app 初始化");
-//        MyEndpointConfigure.context = applicationContext;
-//    }
-
+public class MyEndpointConfigure extends ServerEndpointConfig.Configurator {
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
