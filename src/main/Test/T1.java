754a09d32bf0f9bcd44f25ece89ad718d6023c2c
mjykpzt <44693366+mjykpzt@users.noreply.github.com>
2020-03-26 21:53:48 +0800

    .

---
+++ src/main/Test/T1.java
@@ -0,0 +1,16 @@
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author mjy
+ * @create 2020-03-24-0:12
+ */
+public class T1 {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(bCryptPasswordEncoder.encode("0205"));
+        boolean matches = bCryptPasswordEncoder.matches("0205",
+                "$2a$10$wfP9B8o/gQ/ajdQ.ED87kuqZzOfvbxE9gtmP4MwXdhWJ1FB/VIXxy");
+        System.out.println(matches);
+
+    }
+}
