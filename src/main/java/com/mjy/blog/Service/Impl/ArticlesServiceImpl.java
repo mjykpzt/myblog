5acbe3601ed40221b912db5a30843e7649c29f2f
mjykpzt <44693366+mjykpzt@users.noreply.github.com>
2020-06-05 17:42:08 +0800

    .

--- src/main/java/com/mjy/blog/Service/Impl/ArticlesServiceImpl.java
+++ src/main/java/com/mjy/blog/Service/Impl/ArticlesServiceImpl.java
@@ -30,7 +30,7 @@ import java.util.UUID;
  * @create 2020-03-08-22:55
  */
 @Service
-@Transactional(isolation =Isolation.READ_COMMITTED)
+@Transactional(isolation = Isolation.READ_COMMITTED)
 public class ArticlesServiceImpl implements ArticlesService {
     @Value("${imgPath}")
     private String imgFolderPath;
@@ -44,32 +44,32 @@ public class ArticlesServiceImpl implements ArticlesService {
 
     @Override
     @Transactional(readOnly = true)
-    public ResponseBean findArticlesInformation(Integer uid,Integer iid,String searchName,Integer pageNum,Integer pageSize) {
-        PageHelper.startPage(pageNum,pageSize);
-        List<SysArticles> articles = articlesDao.findArticlesInformation(uid,iid,searchName);
+    public ResponseBean findArticlesInformation(Integer uid, Integer iid, String searchName, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<SysArticles> articles = articlesDao.findArticlesInformation(uid, iid, searchName);
         PageInfo<SysArticles> sysArticlesPageInfo = new PageInfo<>(articles);
-        return ResponseBean.getSuccessResponse("查询成功",sysArticlesPageInfo);
+        return ResponseBean.getSuccessResponse("查询成功", sysArticlesPageInfo);
     }
 
     @Override
     public ResponseBean changeArticles(Articles articles) {
-        if (!itemDao.isCanUse(articles.getItem_id())){
+        if (!itemDao.isCanUse(articles.getItem_id())) {
             return ResponseBean.getFailResponse("该条目已被禁用");
         }
         int OldIid = articlesDao.findIid(articles.getId());
-        if (OldIid!=articles.getItem_id()){
+        if (OldIid != articles.getItem_id()) {
             itemDao.addNumber(articles.getItem_id());
             itemDao.subNumber(OldIid);
         }
 
-        Articles articles1 = articlesXss(articles);
+        Articles articles_safe = articlesXss(articles);
 
-        String s = stripHtml(articles1.getHtml_text());
+        String s = articles_safe.getHtml_text();
         articles.setSource_text(s.substring(0, Math.min(s.length(), 50)));
 
         int i = articlesDao.changeArticleInformation(articles);
         int i1 = articlesDao.changeArticleText(articles);
-        if (i>0&&i1>0){
+        if (i > 0 && i1 > 0) {
             return ResponseBean.getSuccessResponse("改变成功");
         }
         return ResponseBean.getFailResponse("改变失败");
@@ -77,19 +77,19 @@ public class ArticlesServiceImpl implements ArticlesService {
 
     @Override
     public ResponseBean addArticles(Articles articles) {
-        if (!itemDao.isCanUse(articles.getItem_id())){
+        if (!itemDao.isCanUse(articles.getItem_id())) {
             return ResponseBean.getFailResponse("该条目已被禁用");
         }
 
-        Articles articles1 = articlesXss(articles);
+        Articles articles_safe = articlesXss(articles);
 
-        String s = stripHtml(articles1.getHtml_text());
+        String s = articles_safe.getHtml_text();
         articles.setSource_text(s.substring(0, Math.min(s.length(), 50)));
         int i = articlesDao.addArticleInformation(articles);
         int i1 = articlesDao.addArticleText(articles);
         Integer id = articles.getItem_id();
         itemDao.addNumber(id);
-        if (i>0&&i1>0){
+        if (i > 0 && i1 > 0) {
             return ResponseBean.getSuccessResponse("添加文章成功");
         }
         return ResponseBean.getFailResponse("添加文章失败");
@@ -97,21 +97,21 @@ public class ArticlesServiceImpl implements ArticlesService {
 
 
     @Override
-    @Transactional(isolation = Isolation.READ_COMMITTED )
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public ResponseBean findArticlesByAid(Integer aid) {
         SysArticles articles = articlesDao.findArticlesByAid(aid);
-        if (articles != null){
-            articlesDao.ChangeReadNums(aid,1);
-            return ResponseBean.getSuccessResponse("查询成功",articles);
+        if (articles != null) {
+            articlesDao.ChangeReadNums(aid, 1);
+            return ResponseBean.getSuccessResponse("查询成功", articles);
         }
         return ResponseBean.getFailResponse("查询失败");
     }
 
     @Override
-    public ResponseBean delArticle(Integer aid,Integer iid) {
+    public ResponseBean delArticle(Integer aid, Integer iid) {
         int i = articlesDao.delArticle(aid);
         itemDao.subNumber(iid);
-        if (i>0){
+        if (i > 0) {
             return ResponseBean.getSuccessResponse("删除成功");
         }
         return ResponseBean.getFailResponse("删除失败");
@@ -120,8 +120,8 @@ public class ArticlesServiceImpl implements ArticlesService {
     @Override
     public ResponseBean addImg(HttpServletRequest req, MultipartFile image) {
         StringBuilder url = new StringBuilder();
-        String userpath=""+req.getAttribute("username");
-        String path = imgFolderPath+userpath;
+        String userpath = "" + req.getAttribute("username");
+        String path = imgFolderPath + userpath;
         File imgFolder = new File(path);
         if (!imgFolder.exists()) {
             imgFolder.mkdirs();
@@ -135,11 +135,11 @@ public class ArticlesServiceImpl implements ArticlesService {
                 .append("/imgsave/")
                 .append(userpath);
         String imgName = UUID.randomUUID() + "_" + Objects.requireNonNull(image.getOriginalFilename()).replaceAll(" ", "");
-        articlesDao.addImUrl(path+"/"+imgName,(Integer) req.getAttribute("uid"));
+        articlesDao.addImUrl(path + "/" + imgName, (Integer) req.getAttribute("uid"));
         try {
             IOUtils.write(image.getBytes(), new FileOutputStream(new File(imgFolder, imgName)));
             url.append("/").append(imgName);
-            return ResponseBean.getSuccessResponse("图片上传成功",url.toString());
+            return ResponseBean.getSuccessResponse("图片上传成功", url.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -151,25 +151,23 @@ public class ArticlesServiceImpl implements ArticlesService {
         return articlesDao.findAid(aid);
     }
 
-    public String stripHtml(String content) {
-        content = content.replaceAll("<p .*?>", "");
-        content = content.replaceAll("<br\\s*/?>", "");
-        content = content.replaceAll("\\<.*?>", "");
-        return content;
-    }
 
-    private Articles articlesXss(Articles articles){
-        //过滤html，防止xss
+    /**
+     * @param articles
+     * @return: com.mjy.blog.Bean.Articles
+     * @author: 0205
+     * <p>
+     * 过滤html，防止xss
+     */
+    private Articles articlesXss(Articles articles) {
+
         String unsafe_html_text = articles.getHtml_text();
         String html_text = TextXssUtils.FilterXss(unsafe_html_text);
         articles.setHtml_text(html_text);
 
         String unsafe_md_text = articles.getMd_text();
         String md_text = TextXssUtils.FilterXss(unsafe_md_text);
-        System.out.println(md_text);
         articles.setMd_text(md_text);
-        System.out.println("*********************************************************");
-        System.out.println(articles.getMd_text());
 
         return articles;
     }
